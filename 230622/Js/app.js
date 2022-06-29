90b46bffb3e9cbe7063e49628bc0c366cacedb72
Juadebfm <97069600+Juadebfm@users.noreply.github.com>
2022-06-29 04:40:52 -0500

    Scroll active function for NAV-MENU
    Wrote the code for scroll active function

--- 230622/Js/app.js
+++ 230622/Js/app.js
@@ -78,3 +78,26 @@ let swiperTestimonial = new Swiper(".testimonial-container", {
         },
     },
   });
+
+
+//   SCROLL SECTION WHEN ACTIVE
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
